64357ed2c0f226a671711c99cdd2cf9f569ddc98
Navya Raj <navyaraj2003@gmail.com>
2024-10-19 23:18:52 +0530

    added circles

---
+++ src/Components/Home/contact-data.js
@@ -0,0 +1,33 @@
+import c1 from "../../img/c1.jpeg";
+import c2 from "../../img/c2.jpeg";
+import c3 from "../../img/c3.jpeg";
+import c4 from "../../img/c4.jpeg";
+
+export const contactData = [
+  {
+    title: "Women in Business",
+    icon: c1,
+    description:
+      "A community for female entrepreneurs to share advice, network, and grow their brands on Mercari.",
+  },
+  {
+    title: "Vintage Queens",
+    icon: c4,
+    description:
+      "A circle for vintage fashion lovers who cherish unique, timeless pieces and upcycled treasures.",
+  },
+  {
+    title: "Moms who Hustle",
+    icon: c2,
+    description:
+      "Empowering entrepreneurial moms to build their businesses while balancing family and life.",
+  },
+  {
+    title: "EcoFashionistas",
+    icon: c3,
+    description:
+      "Join a community of women passionate about sustainable fashion, swapping style tips and eco-conscious finds.",
+  },
+];
+
+export default contactData;
